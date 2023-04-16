b65b4945cf155dc8f49259449e9cfd7c7bc7e215
AdriUMA <adriantm@uma.es>
2023-04-16 22:33:36 +0200

    Add sender list and docs to address list

---
+++ linkedlist/senderList.c
@@ -0,0 +1,125 @@
+#include "senderList.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void createList(SenderList* senderList){
+    *senderList = NULL;
+}
+
+void deleteList(SenderList* senderList){
+    SenderList previous = NULL;
+    SenderList current = *senderList;
+
+    while (current != NULL) {
+        // Iterate to the next node
+        previous = current;
+        current = current->next;
+
+        // Close Socket and free Sender memory
+        closeSender(previous->sender);
+
+        // Free list memory
+        free(previous);
+    }
+
+    *senderList = NULL;
+}
+
+void insertSender(SenderList* senderList, char* address, unsigned port, unsigned bufferSize){
+    // Sender to add
+    SenderList newSender =  malloc(sizeof(struct str_senderList));
+    if(newSender == NULL) return;
+
+    // New sender inicialization
+    newSender->next = NULL;
+
+    // If the list is empty
+    if(*senderList == NULL){
+        *senderList = newSender;
+
+        // Open socket
+        newSender->sender = openSender(address, port, bufferSize);
+
+        // If fails
+        if(newSender->sender == NULL){
+            // List will keep empty
+            *senderList = NULL;
+
+            // Free memory
+            free(newSender);
+            return;
+        }
+
+    }else{
+        SenderList previous = NULL;
+        SenderList current = *senderList;
+
+        // Iterates all the list
+        while (current != NULL)
+        {
+            // If address exits in the list
+            if(strcmp(current->sender->address, address) == 0){
+                // Free memory                
+                free(newSender);
+                return;
+            }
+
+            previous = current;
+            current = current->next;
+        }
+
+        previous->next = newSender;
+
+        // Open socket
+        newSender->sender = openSender(address, port, bufferSize);
+
+        // If fails
+        if(newSender->sender == NULL){
+            // List will not change
+            previous->next = NULL;
+
+            // Free memory
+            free(newSender);
+            return;
+        }
+
+    }
+}
+
+void removeSender(SenderList* senderList, char* address){
+    if(*senderList == NULL) return;
+
+    SenderList current = *senderList;
+    SenderList previous = current;
+
+    // If first element is the target
+    if(strcmp(current->sender->address, address) == 0){
+        // senderList will point to the second
+        *senderList = current->next;
+
+        // Then, free memory
+        closeSender(current->sender);
+        free(current);
+        return;
+    }else{
+        current = current->next;
+    }
+
+    // Iterates and stop when the address matches
+    while(current != NULL && strcmp(current->sender->address, address) != 0){
+        previous = current;
+        current = current->next;
+    }
+    
+    // If current is equal NULL, no elements has to be removed 
+    if(current != NULL){
+        // Previous point to the next->next
+        previous->next = current->next;
+
+        // Free memory
+        closeSender(current->sender);
+        free(current);
+    }
+}
